b5113baaddbf7c7ac8af16cf6f4e15b52a1b2cf5
ashleygraf101 <ashley.graf101@gmail.com>
2015-09-17 12:14:08 +1000

    Revert "can't see shit"
    This reverts commit 8eeccbd5c9d7a62320af365c258129600dbdbf11.

--- sydneyHouses.js
+++ sydneyHouses.js
@@ -739,33 +739,3 @@ subButton.addEventListener('click', function() {
     printSeventy();
     printEighty();
 });
-
-
-var form  = document.getElementsByTagName('form')[1];
-var location = document.getElementById('locationData');
-var income = document.getElementById('incomeData');
-var error = document.querySelector('.error');
-
-location.addEventListener("keyup", function (event) {
-  // Each time the user types something, we check if the
-  // email field is valid.
-  if (location.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    error.innerHTML = ""; // Reset the content of the message
-    error.className = "error"; // Reset the visual state of the message
-  }
-}, false);
-form.addEventListener("submit", function (event) {
-  // Each time the user tries to send the data, we check
-  // if the email field is valid.
-  if (!location.validity.valid) {
-    
-    // If the field is not valid, we display a custom
-    // error message.
-    error.innerHTML = "I expect an e-mail, darling!";
-    error.className = "error active";
-    // And we prevent the form to be sent by canceling the event
-    event.preventDefault();
-  }
-}, false);
